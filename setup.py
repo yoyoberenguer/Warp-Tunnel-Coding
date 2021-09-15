03a2e0bbdce40cc26d983b33bd53b3dd3b6822ee
Yoann Berenguer <38462796+yoyoberenguer@users.noreply.github.com>
2021-09-15 15:42:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import Cython
 
 # encoding: utf-8
 # USE :
@@ -14,18 +15,9 @@ import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-# /O2 sets a combination of optimizations that optimizes code for maximum speed.
-# /Ot (a default setting) tells the compiler to favor optimizations for speed over optimizations for size.
-# /Oy suppresses the creation of frame pointers on the call stack for quicker function calls.
 setup(
     name='TUNNEL',
     ext_modules=cythonize(Extension(
-            "*", ['*.pyx'], extra_compile_args=["/openmp", "/Qpar", "/fp:fast", "/O2", "/Oy", "/Ot"], language="c",
-             
-        )
-    ),
-    include_dirs=[numpy.get_include()],
-   
-
-
+            "*", ['*.pyx'], language="c")),
+    include_dirs=[numpy.get_include()])
 
